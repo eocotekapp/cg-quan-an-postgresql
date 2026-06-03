773c3c4c6925ca0f154715a2cf74d6fdfd8351d1
eocotekapp <smy763574@gmail.com>
2026-06-03 19:14:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // CG Quán Ăn config
 // Dán URL Cloudflare Tunnel API Android vào đây, ví dụ:
 // window.CG_API_BASE_URL = "https://abc.trycloudflare.com";
-window.CG_API_BASE_URL = window.CG_API_BASE_URL || "https://oregon-wellness-bumper-continent.trycloudflare.com";
+window.CG_API_BASE_URL = window.CG_API_BASE_URL || "https://thanksgiving-dragon-packing-prior.trycloudflare.com";
